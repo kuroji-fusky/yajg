f1bcc9ae13ea892dc7cc442b05bd3baca33e2dd9
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-02-07 16:07:46 +0800

    remove pwa; export module broken

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import type { UserConfig } from 'vite'
-import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 const config: UserConfig = {
-	plugins: [sveltekit(), SvelteKitPWA()]
+	plugins: [sveltekit()]
 }
 
 export default config

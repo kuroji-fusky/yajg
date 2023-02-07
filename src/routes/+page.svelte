d33c4ef4b78639a873c19f37b65a7c2367bfc870
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-02-08 07:53:21 +0800

    add sidebar resize logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,36 @@
 <script lang="ts">
+	import { onMount } from 'svelte'
+
 	let output = []
 
 	// Resizable sidebar logic
+	let appContainer: HTMLElement
+	let grippy: HTMLElement
+
+	onMount(() => {
+		function handleGrippy({ clientX }: MouseEvent) {
+			let pos = clientX
+			if (pos < 0) pos = 0
+
+			let mouseX = `${pos}px`
+
+			appContainer.style.setProperty('--editor-sidebar-width', mouseX)
+		}
+
+		grippy.onmousedown = () => {
+			window.addEventListener('mousemove', handleGrippy)
+		}
+
+		window.onmouseup = () => {
+			window.removeEventListener('mousemove', handleGrippy)
+		}
+	})
 </script>
 
-<div class="w-screen flex h-fit">
+<div class="w-screen flex h-fit" bind:this={appContainer}>
 	<div class="block-sidebar-container">
-		<div>Component blocks here</div>
-		<div id="grip" />
+		<div class="overflow-hidden">Component blocks here</div>
+		<div id="grip" bind:this={grippy} />
 	</div>
 	<div class="code-output-wrapper">Code output here</div>
 </div>
@@ -17,11 +40,18 @@
 		height: calc(100vh - 7.65rem);
 	}
 
+	.block-sidebar-container,
+	.code-output-wrapper {
+		@apply px-5 py-4;
+	}
+
 	.block-sidebar-container {
 		@apply relative pr-2.5;
 
+		width: var(--editor-sidebar-width, 320px);
+
 		#grip {
-			@apply w-1 hover:w-1.5 bg-slate-500 absolute top-0 right-0 bottom-0 cursor-ew-resize;
+			@apply w-1 bg-slate-500 absolute top-0 -right-1.5 bottom-0 cursor-ew-resize;
 		}
 	}
 </style>
